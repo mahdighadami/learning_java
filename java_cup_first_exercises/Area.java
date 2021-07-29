ea06fa30fd521afd68bcf752008951285e0522d0
Mahdi <mahdighadami2@gmail.com>
2021-07-29 23:22:17 +0430

    in "java_cup_first_exercisess" directory, add Area class: ".java" and ".class" format.

---
+++ java_cup_first_exercises/Area.java
@@ -0,0 +1,9 @@
+public class Area {
+    public static void main(String[] argmnt){
+        System.out.println(area(1));
+    }
+    static double area(double radius){
+        return radius * radius * 3.1415;
+
+    }
+}
