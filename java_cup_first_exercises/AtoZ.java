e9feb79cf0cdb53d0c7d868d4cef411e5217285d
Mahdi <mahdighadami2@gmail.com>
2021-07-30 18:41:42 +0430

    in "java_cup_first_exercisess" directory, add AtoZ.java

---
+++ java_cup_first_exercises/AtoZ.java
@@ -0,0 +1,10 @@
+public class AtoZ {
+    public static void main(String[] argmnt){
+
+        for (int i = 97; i < 123; i++){
+            if (i % 2 == 1){
+                System.out.println(Character.toString(i));
+            }
+        }
+    }
+}
