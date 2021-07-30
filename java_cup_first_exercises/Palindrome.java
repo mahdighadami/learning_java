9656de50171b0a26eefd598ee8c414c0e51ce7b6
Mahdi <mahdighadami2@gmail.com>
2021-07-30 18:46:45 +0430

    in "java_cup_first_exercisess" directory, add Palindrome.java

---
+++ java_cup_first_exercises/Palindrome.java
@@ -0,0 +1,22 @@
+public class Palindrome {
+
+    public static void main(String[] argmnt){
+        System.out.println(palindrome(211112));
+    }
+
+    static boolean palindrome(int n){
+
+        int number = n;
+        int a = 0;
+
+        while (number != 0){
+            int remaining = number % 10;
+            a *= 10;
+            a += remaining;
+            number -= remaining;
+            number /= 10;
+        }
+
+        return a == n;
+    }
+}
