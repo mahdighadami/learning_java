fd5d30bf5385318fe5c8beeb34667a63bdd2c768
Mahdi <mahdighadami2@gmail.com>
2021-08-05 23:39:28 +0430

    get input in "Area.java" for make a complete console program.

--- java_cup_first_exercises/Area.java
+++ java_cup_first_exercises/Area.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 public class Area {
-    public static void main(String[] argmnt){
-        System.out.println(area(1));
+    public static void main(String[] argument) {
+        System.out.println("Enter the radius:");
+        Scanner scanner = new Scanner(System.in);
+        double n = scanner.nextDouble();
+        System.out.println(area(n));
+
     }
     static double area(double radius){
         return radius * radius * 3.1415;
