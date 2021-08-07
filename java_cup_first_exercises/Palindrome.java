c58f8db4bad28d249ded29e4a24a2f786452d1f9
Mahdi <mahdighadami2@gmail.com>
2021-08-07 22:07:52 +0430

    get input in "Palindrome.java" for make a complete console program.

--- java_cup_first_exercises/Palindrome.java
+++ java_cup_first_exercises/Palindrome.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
 public class Palindrome {
 
     public static void main(String[] argmnt){
-        System.out.println(palindrome(211112));
+
+        System.out.println("Enter the number:");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        System.out.println(palindrome(n));
     }
 
     static boolean palindrome(int n){
