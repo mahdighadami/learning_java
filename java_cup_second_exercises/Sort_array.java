4fb2751e3b493c9842f4e4736e15ad35cd0393f8
Mahdi <mahdighadami2@gmail.com>
2021-08-13 22:25:26 +0430

    in "java_cup_second_exercises" directory, add Sort_array.java

---
+++ java_cup_second_exercises/Sort_array.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+public class Sort_array {
+
+    public static void main(String[] argument){
+        long[] array = {3, 1, -4, 63, -16, 28};
+        System.out.println(Arrays.toString(sortfunc(array)));
+    }
+
+    public static long[] sortfunc(long[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            int index = i;
+            for (int j = i + 1; j < n; j++) {
+                if (array[j] < array[index]) {
+                    index = j;
+                }
+            }
+            long temp = array[index];
+            array[index] = array[i];
+            array[i] = temp;
+        }
+        return array;
+    }
+}
